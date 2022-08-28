f5bf3e61d27f959cdcd91a764f0966491be264fa
Carsten Schlegel <cschle@gmail.com>
2022-08-29 00:40:33 +0200

    0.1.3

--- tutorial.ps1
+++ tutorial.ps1
@@ -16,7 +16,9 @@ function init
     del ./GitProjekt -Recurse -Force
     git clone https://github.com/bugfrei/GitProjekt
     cd GitProjekt
-
+    git remote add github https://github.com/bugfrei/GitProjekt
+    git reset --hard 16d586f
+    git push origin +main --force
     cls
     hilfe
 }
@@ -41,17 +43,20 @@ function test
 function hilfe
 {
     cls
-    Write-Host "Es erfolgt jetzt eine Initialisierung des Tutorials. Diese kann jederzeit mit run init erneut durchgeführt werden."
+    Write-Host "Es erfolgt jetzt eine Initialisierung des Tutorials. Diese kann jederzeit mit run init erneut durchgefuehrt werden."
     Write-Host
     Write-Host "Es gibt folgende run <Kommandos>:"
     Write-Host "run hilfe : zeigt diese Hilfe an."
-    Write-Host "run todo: öffnet die todo.md Datei (PowerShell Code im Browser, ansonsten im verknüpften Programm z.B. VS Code)"
-    Write-Host "run init: Initialisiert das Tutorial neu"
+    Write-Host "run todo: oeffnet die todo.md Datei (PowerShell Code im Browser, ansonsten im verknuepften Programm z.B. VS Code)"
+    Write-Host "run init: Initialisiert das Tutorial neu *"
     Write-Host
     Write-Host "Markdown anzeigen in Visual Studio Code:" -ForegroundColor Yellow
-    Write-Host "F1 -> Markdown: Vorschau öffnen"
+    Write-Host "F1 -> Markdown: Vorschau oeffnen"
     Write-Host "Oder einfach Strg+Shift+V"
     Write-Host
+    Write-Host "* Bei der Initialisierung werden auch alle gepush'te commits geloescht."
+    Write-Host
+    Write-Host "Version: 0.1.3" -ForegroundColor DarkGray
 
 }
 
